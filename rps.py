859f04e980b9e53c486ba79755cb7c205743bb4b
brandon <b.mazey@nyu.edu>
2026-03-15 12:44:36 -0400

    rps

--- rps.py
+++ rps.py
@@ -1,3 +1,4 @@
+import random
 import time
 
 def print_dramatic_text(text: str, delay=0.1):
@@ -7,4 +8,17 @@ def print_dramatic_text(text: str, delay=0.1):
     print()
 
 if __name__ == '__main__':
-    print_dramatic_text('welcome to rock, paper, scissors! 🪨📃✂️')
+    print_dramatic_text('welcome to Mr. Mazey\'s rock, paper, scissors! 🪨📃✂️')
+
+    score = 0
+    options = ['rock', 'paper', 'scissors']
+
+    i = 0
+    while i < 3:
+        choice = ''
+        while choice != 'rock' or choice != 'paper' or choice != 'scissors':
+            choice = input('enter your choice (rock, paper, scissors): ')
+
+        opponent = options[random.randint(0, 2)]
+        print_dramatic_text('rock ... paper ... scissors ... shoot!')
+
