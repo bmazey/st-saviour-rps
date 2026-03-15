10207887e8730ba3f31782fba92c03f19b6cf0b0
brandon <b.mazey@nyu.edu>
2026-03-15 12:36:18 -0400

    foundations

---
+++ rps.py
@@ -0,0 +1,10 @@
+import time
+
+def print_dramatic_text(text: str, delay=0.1):
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(delay)
+    print()
+
+if __name__ == '__main__':
+    print_dramatic_text('welcome to rock, paper, scissors! 🪨📃✂️')
