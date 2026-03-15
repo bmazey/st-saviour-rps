0f9b2cdc64d7ab9846d0d350404865635331088b
brandon <b.mazey@nyu.edu>
2026-03-15 13:01:57 -0400

    rock emoji

--- rps.py
+++ rps.py
@@ -8,17 +8,45 @@ def print_dramatic_text(text: str, delay=0.1):
     print()
 
 if __name__ == '__main__':
-    print_dramatic_text('welcome to Mr. Mazey\'s rock, paper, scissors! 🪨📃✂️')
+    print_dramatic_text('welcome to Mr. Mazey\'s rock, paper, scissors! 🗿📃✂️')
 
     score = 0
-    options = ['rock', 'paper', 'scissors']
+    options = ['🗿', '📃', '✂️']
 
     i = 0
     while i < 3:
         choice = ''
-        while choice != 'rock' or choice != 'paper' or choice != 'scissors':
+        while choice != 'rock' and choice != 'paper' and choice != 'scissors':
             choice = input('enter your choice (rock, paper, scissors): ')
+
+        if choice == 'rock':
+            choice = '🗿'
+        if choice == 'paper':
+            choice = '📃'
+        if choice == 'scissors':
+            choice = '✂️'
 
         opponent = options[random.randint(0, 2)]
         print_dramatic_text('rock ... paper ... scissors ... shoot!')
 
+        if choice == opponent:
+            print(f'you chose {choice} and opponent chose {opponent}, you tie!')
+        if choice == '🗿' and opponent == '✂️':
+            print(f'you chose {choice} and opponent chose {opponent}, you win!')
+            score += 1
+        if choice == '🗿' and opponent == '📃':
+            print(f'you chose {choice} and opponent chose {opponent}, you lose!')
+        if choice == '📃' and opponent == '🗿':
+            print(f'you chose {choice} and opponent chose {opponent}, you win!')
+            score += 1
+        if choice == '📃' and opponent == '✂️':
+            print(f'you chose {choice} and opponent chose {opponent}, you lose!')
+        if choice == '✂️' and opponent == '📃':
+            print(f'you chose {choice} and opponent chose {opponent}, you win!')
+            score += 1
+        if choice == '✂️' and opponent == '🗿':
+            print(f'you chose {choice} and opponent chose {opponent}, you lose!')
+        
+        i += 1
+
+    print_dramatic_text(f'congratulations you scored {score}/3!')
