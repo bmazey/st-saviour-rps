3ff0133aff17d54502d6af4730195c3b76bc2289
brandon mazey <brandon@brandons-MacBook-Air.local>
2026-03-25 08:39:06 -0400

    rps

--- rps.py
+++ rps.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-def print_dramatic_text(text: str, delay=0.1):
+def print_dramatic_text(text: str, delay=0.05):
     for char in text:
         print(char, end='', flush=True)
         time.sleep(delay)
@@ -9,3 +9,24 @@ def print_dramatic_text(text: str, delay=0.1):
 
 if __name__ == '__main__':
     print_dramatic_text('welcome to rock, paper, scissors! 🗿📃✂️')
+
+    options = ['rock', 'paper', 'scissors']
+    choice = random.randint(0, 2)
+
+    computer = options[choice]
+
+    print('computer player selected: ' + options[choice])
+
+    response = input('please choose rock, paper, or scissors: ')
+
+    while response != 'rock' and response != 'paper' and response != 'scissors':
+        response = input('please choose rock, paper, or scissors: ')
+
+    print(f'player selected {response}')
+
+    if response == 'rock' and computer == 'rock':
+        print('you tie!')
+    if response == 'rock' and computer == 'scissors':
+        print('you win!')
+    if response == 'rock' and computer == 'paper':
+        print('you lose!')
